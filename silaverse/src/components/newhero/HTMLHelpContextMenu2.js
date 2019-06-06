aa0b753c5c2532d712ba291ef338064c6677ff0d
MEBonham <programmer@mckaybonham.com>
2019-06-06 17:04:27 -0600

    More adjustment of HTML-help context menu

--- silaverse/src/components/newhero/HTMLHelpContextMenu2.js
+++ silaverse/src/components/newhero/HTMLHelpContextMenu2.js
@@ -33,25 +33,45 @@ const HTMLHelpContextMenu2 = props => {
     return(
         <Menu id={props.menuId}>
             <Submenu label="Insert Into Textarea" arrow="&#9656;">
+                <Item onClick={onClick} data={{
+                    type: "insert",
+                    input: props.input,
+                    payload: `<p><strong></strong> () <strong>&middot;</strong> <span class="lesser-note"> ppt</span><br />\n</p>`
+                }}>
+                    Paragraph Beginning
+                </Item>
+                <Item onClick={onClick} data={{
+                    type: "insert",
+                    input: props.input,
+                    payload: `<ul>\n<li><strong></strong> () <strong>&middot;</strong> <span class="lesser-note"> ppt</span><br />\n</li>\n</ul>`
+                }}>
+                    List Beginning
+                </Item>
                 <Item onClick={onClick} data={{ type: "insert", input: props.input, payload: `<strong></strong>` }}>
                     Bold Tag
                 </Item>
                 <Item onClick={onClick} data={{ type: "insert", input: props.input, payload: `<strong>&middot;</strong>` }}>
                     Bold Middot
                 </Item>
-                <Item onClick={onClick} data={{ type: "insert", input: props.input, payload: `<span class=""></span>` }}>
-                    Generic span-Tag
+                <Item onClick={onClick} data={{
+                    type: "insert",
+                    input: props.input,
+                    payload: `<p class="alt"><span class="h3-style">:</span> <strong>&middot;</strong> <span class="cost-note"> ppt</span><br />\n<span class="lesser-note"> Power</span></p>`
+                }}>
+                    ALT Power Beginning
                 </Item>
-                <Item onClick={onClick} data={{ type: "insert", input: props.input, payload: `<ul>\n<li><strong> &middot;</strong> <span class="lesser-note"> ppt</span></li>\n</ul>` }}>
-                    List Beginning
-                </Item>
-                <Item onClick={onClick} data={{ type: "insert", input: props.input, payload: `<p><strong> &middot;</strong> <span class="lesser-note"> ppt</span></p>` }}>
-                    Paragraph Beginning
+                <Item onClick={onClick} data={{
+                    type: "insert",
+                    input: props.input,
+                    payload: `<ul>\n<li><strong>:</strong> + <strong>&middot;</strong> Damage DC 15 (Strength-based), bludgeoning <strong>&middot;</strong> .<br />\n</li>\n</ul>`
+                }}>
+                    Attack Beginning
                 </Item>
             </Submenu>
             <Separator />
             <Submenu label="Copy to Clipboard" arrow="&#9656;">
                 <Item onClick={onClick} data={{ type: "copy", payload: `<span class="lesser-note"></span>` }}>Lesser Note Span</Item>
+                <Item onClick={onClick} data={{ type: "copy", payload: `<span class=""></span>` }}>Generic span-Tag</Item>
                 <Item onClick={onClick} data={{ type: "copy", payload: ` &middot; ` }}>Spaced Out Middot</Item>
             </Submenu>
         </Menu>
