35820fa26c652e2f293eabec43ef21b141e4c49c
MEBonham <programmer@mckaybonham.com>
2019-06-10 09:05:26 -0600

    Organization displaying, getting ready to add new folders to db

--- silaverse/src/components/organization/OrganizeHeroes.js
+++ silaverse/src/components/organization/OrganizeHeroes.js
@@ -1,6 +1,10 @@
-import React from 'reactn';
+import React, { useState, useEffect, useGlobal } from 'reactn';
 
 import firebase from '../../fbConfig';
+
+import useForm from '../../hooks/useForm';
+
+import '../../css/Organization.css';
 
 const OrganizeHeroes = props => {
 
@@ -10,9 +14,64 @@ const OrganizeHeroes = props => {
         }
     });
 
+    const [ orgObject, setOrgObject ] = useState({});
+    const [ heroes ] = useGlobal('heroes');
+
+    const db = firebase.db;
+    useEffect(() => {
+        db.collection("folders").get()
+            .then(querySnapshot => {
+                querySnapshot.forEach(doc => {
+                    // orgObject[doc.data().name] = JSON.parse(doc.data().heroes);
+                    setOrgObject({
+                        ...orgObject,
+                        [doc.data().name]: JSON.parse(doc.data().heroes)
+                    })
+                });
+            })
+            .catch(err => {
+                console.log("Error initializing folders:", err);
+            });
+    }, []);
+
+    const saveNewFolder = () => {
+        console.log("Time to add a new folder to the db.");
+    }
+
+    const { inputs, setInputs, handleInputChange, handleSubmit } = useForm(saveNewFolder);
+
     return(
         <section className="organization-envelope">
-            
+            <h1>Organize Heroes</h1>
+            {Object.keys(orgObject).map(folderName => (
+                <section key={folderName}>
+                    <h2>{folderName}</h2>
+                    <ul>
+                        {orgObject[folderName].map(heroId => {
+                            const hero = heroes.filter(heroObj => (heroObj.id === heroId))[0];
+                            return(<li key={heroId}>{hero.name}</li>);
+                        })}
+                    </ul>
+                </section>
+            ))}
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="newFolderInput">Add New Folder:</label>
+                <input
+                    type="text"
+                    id="newFolderInput"
+                    value={inputs.newFolderInput || ""}
+                    onChange={handleInputChange}
+                />
+                <button type="submit">Save Folder</button>
+            </form>
+            <section>
+                <h2>Uncategorized</h2>
+                <ul>
+                    {heroes ? heroes.filter(hero => (hero.folder === undefined)).map(heroObj => (
+                        <li key={heroObj.id}>{heroObj.name}</li>
+                    )) : null}
+                </ul>
+            </section>
         </section>
     )
 }
