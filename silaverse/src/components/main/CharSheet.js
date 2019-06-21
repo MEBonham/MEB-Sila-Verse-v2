09b6211a8c91e3a808a2f58908a89ab27e309a5f
MEBonham <programmer@mckaybonham.com>
2019-06-21 06:45:22 -0600

    Basic tab select for hero forms implemented

--- silaverse/src/components/main/CharSheet.js
+++ silaverse/src/components/main/CharSheet.js
@@ -2,8 +2,11 @@ import React, { useState, useEffect, useGlobal } from 'reactn';
 import { Link } from 'react-router-dom';
 import DOMPurify from 'dompurify';
 
+import firebase from '../../fbConfig';
+
 import { PptTotalsProvider } from '../../hooks/PptTotalsContext';
 
+import { packageHeroForGlobal } from '../edithero/EditHelperFcts';
 import AbilitiesSection from './AbilitiesSection';
 import PowersSection from './PowersSection';
 import AdvantagesSection from './AdvantagesSection';
@@ -28,6 +31,33 @@ const CharSheet = props => {
     if (filteredHeroes.length && thisHero !== filteredHeroes[0]) {
         setThisHero(filteredHeroes[0]);
     }
+
+    const [ activeForm, setActiveForm ] = useState(0);
+    const [ hasForms, setHasForms ] = useState(false);
+    const [ thisHeroPrime, setThisHeroPrime ] = useState(null);
+    useEffect(() => {
+        if (thisHero && thisHero.forms && thisHero.forms.length) {
+            setHasForms(true);
+            if (activeForm > 0) {
+                const db = firebase.db;
+                const formName = thisHero.forms[activeForm - 1];
+                console.log(`${thisHero.urlid}.${formName}`);
+                db.collection("forms").doc(`${thisHero.urlid}.${formName}`)
+                    .get()
+                    .then(doc => {
+                        // console.log(doc);
+                        setThisHeroPrime(packageHeroForGlobal(`${thisHero.urlid}.${formName}`, doc.data()));
+                    })
+                    .catch(err => {
+                        console.log("Error retrieving form from db:", err);
+                    });
+            } else {
+                setThisHeroPrime(thisHero);
+            }
+        } else if (thisHero) {
+            setThisHeroPrime(thisHero);
+        }
+    }, [ thisHero, activeForm ]);
     
     const [ finalTotal, setFinalTotal ] = useState(0);
     const [ pptTotals, setPptTotals ] = useState({
@@ -45,43 +75,69 @@ const CharSheet = props => {
         setFinalTotal(totalVar);
     }, [ pptTotals ]);
 
-    if (thisHero) {
+    const handleTabSelect = ev => {
+        // console.log(ev.target.id);
+        setActiveForm(parseInt(ev.target.id.split("-")[2]));
+    }
+
+    if (thisHeroPrime) {
         let heroTypeEl;
-        if (thisHero.subHero && thisHero.subHero.length) {
-            const subHeroSplit = thisHero.subHero.split("\\");
+        if (thisHeroPrime.subHero && thisHeroPrime.subHero.length) {
+            const subHeroSplit = thisHeroPrime.subHero.split("\\");
             const toString = `./${DOMPurify.sanitize(subHeroSplit[1])}`;
-            heroTypeEl = <p className="hero-type">{thisHero.heroType} <Link to={toString}>{subHeroSplit[0]}</Link></p>;
-        } else if (thisHero.heroType && thisHero.heroType.length) {
-            heroTypeEl = <p className="hero-type">{thisHero.heroType}</p>;
+            heroTypeEl = <p className="hero-type">{thisHeroPrime.heroType} <Link to={toString}>{subHeroSplit[0]}</Link></p>;
+        } else if (thisHeroPrime.heroType && thisHeroPrime.heroType.length) {
+            heroTypeEl = <p className="hero-type">{thisHeroPrime.heroType}</p>;
         } else {
             heroTypeEl = null;
+        }
+        let tabDiv;
+        if (hasForms) {
+            tabDiv = (
+                <nav className="char-sheet-tabs">
+                    <div className="char-sheet-tab" id="form-tab-0" onClick={handleTabSelect}>
+                        <label id="form-tab-0">{thisHero.urlid}</label>
+                    </div>
+                    {thisHero.forms && thisHero.forms.length && thisHero.forms.map((formName, i) => {
+                        // const styleStr = `width: calc(100% / ${thisHero.forms.length});`;
+                        return(<div key={i + 1} className="char-sheet-tab" id={`form-tab-${i + 1}`} onClick={handleTabSelect}>
+                            <label id={`form-tab-${i + 1}`}>{formName}</label>
+                        </div>);
+                    })}
+                </nav>
+            );
+        } else {
+            tabDiv = null;    
         }
         return(
             <PptTotalsProvider value={{ pptTotals, setPptTotals }}>
                 <div className="char-sheet-envelope">
-                    <section className="char-sheet">
-                        <header>
-                            <h1>{thisHero.name}</h1>
-                            <h2>{thisHero.identity}</h2>
-                            {heroTypeEl}
-                            <p className="last-header-line">Power Level {thisHero.powerLevel} ({finalTotal} ppt)</p>
-                        </header>
-                        <AbilitiesSection hero={thisHero} />
-                        <PowersSection hero={thisHero} />
-                        <AdvantagesSection hero={thisHero} />
-                        <SkillsSection hero={thisHero} />
-                        <DefensesSection hero={thisHero} />
-                        <OffenseSection hero={thisHero} />
-                        <ComplicationsSection hero={thisHero} />
-                        <section className="pointTotals">
-                            <h3><strong>ABILITIES</strong> [ {pptTotals.abilities} ] + <strong>SKILLS</strong> [ {pptTotals.skills} ] 
-                                + <strong>ADVANTAGES</strong> [ {pptTotals.advantages} ] + <strong>POWERS</strong> [ {pptTotals.powers} ] 
-                                + <strong>DEFENSES</strong> [ {pptTotals.defenses} ]</h3>
-                            <h2><strong>= {finalTotal} PPT TOTAL</strong></h2>
+                    <div className={hasForms ? "char-sheet-paper has-tabs" : "char-sheet-paper"}>
+                        {tabDiv}
+                        <section className="char-sheet">
+                            <header>
+                                <h1>{thisHeroPrime.name}</h1>
+                                <h2>{thisHeroPrime.identity}</h2>
+                                {heroTypeEl}
+                                <p className="last-header-line">Power Level {thisHeroPrime.powerLevel} ({finalTotal} ppt)</p>
+                            </header>
+                            <AbilitiesSection hero={thisHeroPrime} />
+                            <PowersSection hero={thisHeroPrime} />
+                            <AdvantagesSection hero={thisHeroPrime} />
+                            <SkillsSection hero={thisHeroPrime} />
+                            <DefensesSection hero={thisHeroPrime} />
+                            <OffenseSection hero={thisHeroPrime} />
+                            <ComplicationsSection hero={thisHeroPrime} />
+                            <section className="pointTotals">
+                                <h3><strong>ABILITIES</strong> [ {pptTotals.abilities} ] + <strong>SKILLS</strong> [ {pptTotals.skills} ] 
+                                    + <strong>ADVANTAGES</strong> [ {pptTotals.advantages} ] + <strong>POWERS</strong> [ {pptTotals.powers} ] 
+                                    + <strong>DEFENSES</strong> [ {pptTotals.defenses} ]</h3>
+                                <h2><strong>= {finalTotal} PPT TOTAL</strong></h2>
+                            </section>
+                            <BioSection hero={thisHeroPrime} />
                         </section>
-                        <BioSection hero={thisHero} />
-                    </section>
-                    <NotesSection hero={thisHero} />
+                    </div>
+                    <NotesSection hero={thisHeroPrime} />
                 </div>
             </PptTotalsProvider>
         );
